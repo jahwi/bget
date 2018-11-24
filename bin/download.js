4bce1e482a8e905e63b420dd6e6491b0545707cb
jahwi <jawandoyanju@gmail.com>
2018-11-24 09:52:38 +0100

    Create download.js

---
+++ bin/download.js
@@ -0,0 +1,26 @@
+var url = WScript.Arguments(0),
+  filename = WScript.Arguments(1),
+  fso = WScript.CreateObject('Scripting.FileSystemObject'),
+  request, stream;
+if (fso.FileExists(filename)) {
+  WScript.Echo('Already got ' + filename);
+} else {
+  WScript.Echo('Downloading ' + url);
+  request = WScript.CreateObject('MSXML2.ServerXMLHTTP');
+  request.open('GET', url, false); // not async
+  request.send();
+  if (request.status === 200) { // OK
+    WScript.Echo("Size: " + request.getResponseHeader("Content-Length") + " bytes");
+    stream = WScript.CreateObject('ADODB.Stream');
+    stream.Open();
+    stream.Type = 1; // adTypeBinary
+    stream.Write(request.responseBody);
+    stream.Position = 0; // rewind
+    stream.SaveToFile(filename, 1); // adSaveCreateNotExist
+    stream.Close();
+  } else {
+    WScript.Echo('Failed');
+    WScript.Quit(1);
+  }
+}
+WScript.Quit(0);
