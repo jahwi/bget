1dab05f479e1c1c8dfe5cd7a2b4fff0d5af9bf9a
jahwi <jawandoyanju@gmail.com>
2018-11-24 18:57:54 +0100

    Update bget.bat

--- bget.bat
+++ bget.bat
@@ -10,7 +10,6 @@ if not exist temp md temp
 if not exist scripts md scripts
 
 ::init global vars and settings
-mode 120,30
 set info_mode=off
 set list_location=https://raw.githubusercontent.com/jahwi/bget/list/master.txt
 goto :main
@@ -27,7 +26,9 @@ if "!missing_curl!"=="yes" (
 	if "!errorlevel!"=="2" exit /b
 	if "!errorlevel!"=="1" (
 		if not exist curl md curl
-		call :download -js "https://archive.org/download/batchget_gmail_Curl/curl.exe#%cd%\curl\curl.exe https://archive.org/download/batchget_gmail_Curl/curl-ca-bundle.crt#%cd%\curl\curl-ca-bundle.crt https://archive.org/download/batchget_gmail_Curl/libcurl.dll#%cd%\curl\libcurl.dll"
+		call :download -js "https://archive.org/download/batchget_gmail_Curl/curl.exe#%cd%\curl\curl.exe"
+		call :download -js "https://archive.org/download/batchget_gmail_Curl/curl-ca-bundle.crt#%cd%\curl\curl-ca-bundle.crt"
+		call :download -js "https://archive.org/download/batchget_gmail_Curl/libcurl.dll#%cd%\curl\libcurl.dll"
 		set missing_curl_download=
 		for %%a in ("curl\curl.exe" "curl\curl-ca-bundle.crt" "curl\libcurl.dll" ) do ( if not exist "%%~a" set missing_curl_download=yes )
 		if "!missing_curl_download!"=="yes" echo An error occured when downloading curl. && exit /b
@@ -403,19 +404,20 @@ exit
 
 :download
 
+
 ::BITSADMIN download function
-if /i "%1"=="-bits" (
+if /i "%~1"=="-bits" (
 	set /a rnd=%random%
-	set bits_string=%~2
-	bitsadmin /transfer Bget!rnd! /download /priority HIGH !bits_string:#= !
+	for /f "tokens=1,2 delims=#" %%w in ("%~2") do (
+		bitsadmin /transfer Bget!rnd! /download /priority HIGH "%%w" "%%x"
+	)
 	set rnd=
 	set bits_string=
 	exit /b
 )
-
 ::Jscript download function.
 ::Download.js was made by jsjoberg @ https://gist.github.com/jsjoberg/8203376
-if /i "%1"=="-js" (
+if /i "%~1"=="-js" (
 	if not exist "bin\download.js" (
 		choice /c yn /n /m  "Bget's JS download function could not be found. Download? [(Y)es/(N)o]"
 		if "!errorlevel!"=="2" exit /b
@@ -427,35 +429,33 @@ if /i "%1"=="-js" (
 
 	)
 	if exist "bin\download.js" (
-		for %%x in (%~2) do (
-			set jsstring=%%x
-			cscript //NoLogo //e:Jscript bin\download.js !jsstring:#= !
-			set jsstring=
+		for /f "tokens=1,2 delims=#" %%e in ("%~2") do (
+			cscript //NoLogo //e:Jscript bin\download.js "%%e" "%%f"
 		)
-		exit /b
 	)
+	exit /b
 )
 ::Curl download function
-if /i "%1"=="-curl" (
+if /i "%~1"=="-curl" (
 	call :checkcurl
 	if "!missing_curl!"=="yes" exit /b
 	for /f "tokens=1,2 delims=#" %%b in ("%~2") do (
-		curl -s %%b -o %%c
+		curl -s "%%b" -o "%%c"
 	)
 exit /b
 )
 
 ::powershell download function
-if /i "%1"=="-ps" (
+if /i "%~1"=="-ps" (
 	for /f "tokens=1,2 delims=#" %%b in ("%~2") do (
-	powershell -Command wget %%b -OutFile %%c
+	powershell -Command wget "%%b" -OutFile "%%c"
 	)
 	exit /b
 )
 
 ::vbs thingy
 ::vbs script gotten from http://semitwist.com/articles/article/view/downloading-files-from-plain-batch-with-zero-dependencies
-if /i "%1"=="-vbs" (
+if /i "%~1"=="-vbs" (
 	if not exist "bin\download.vbs" (
 		choice /c yn /n /m  "Bget's VBS download function could not be found. Download? [(Y)es/(N)o]"
 		if "!errorlevel!"=="2" exit /b
@@ -467,10 +467,8 @@ if /i "%1"=="-vbs" (
 
 	)
 	if exist "bin\download.vbs" (
-		for %%x in (%~2) do (
-			set vbsstring=%%x
-			cscript //NoLogo //e:VBscript bin\download.vbs !vbsstring:#= !
-			set vbsstring=
+		for /f "tokens=1,2 delims=#" %%e in ("%~2") do (
+			cscript //NoLogo //e:VBScript bin\download.vbs "%%e" "%%f"
 		)
 		exit /b
 	)
@@ -482,5 +480,3 @@ set var_to_clean=%*
 for %%t in (!var_to_clean!) do (set %%t=)
 set var_to_clean=
 exit /b
-
-
