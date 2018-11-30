4222ba6046872adc1be751ac44f9857e7eed2d69
Jahwi <jawandoyanju@gmail.com>
2018-11-30 01:44:36 +0100

    Update upgrade.bat

--- upgrade/upgrade.bat
+++ upgrade/upgrade.bat
@@ -9,20 +9,20 @@ set upgrade_script_location=https://raw.githubusercontent.com/jahwi/bget/master/
 set bget_location=https://raw.githubusercontent.com/jahwi/bget/master/bget.bat
 set changelog_location=https://raw.githubusercontent.com/jahwi/bget/master/docs/changelog.txt
 set readme_location=https://raw.githubusercontent.com/jahwi/bget/master/docs/readme.txt
-set /p upgrade_method=<%~nx0:upgrade_method
-set /p force_bool=<%~nx0:force_bool
+set /p upgrade_method=<"%~dp0\%~nx0:upgrade_method"
+set /p force_bool=<"%~dp0\%~nx0:force_bool"
 if not defined upgrade_method set upgrade_method=bits
 echo Updating...
 
 ::get bget's file hash
 set /a sess_rand=%random%
-call :download -!upgrade_method! "!upgrade_hash_location!#%cd%\temp\hash!sess_rand!.txt"
-if not exist temp\hash!sess_rand!.txt echo Failed to get the upgrade hash. && pause && exit /b
-set/p new_upgrade_hash=<temp\hash!sess_rand!.txt
+call :download -!upgrade_method! "!upgrade_hash_location!#%~dp0\temp\hash!sess_rand!.txt"
+if not exist "%~dp0\temp\hash!sess_rand!.txt" echo Failed to get the upgrade hash. && pause && exit /b
+set/p new_upgrade_hash=<"%~dp0\temp\hash!sess_rand!.txt"
 
-if not exist bin\hash.txt (
+if not exist "%~dp0\bin\hash.txt" (
 	echo No local hash found. Will upgrade anyway.
-	echo %random%%random%%random%>bin\hash.txt
+	echo %random%%random%%random%>"%~dp0\bin\hash.txt"
 )
 
 ::compare old and new hashes
@@ -30,38 +30,38 @@ if not exist bin\hash.txt (
 ::force if switch is applied
 if /i "!force_bool!"=="yes" (
 	echo Forcing upgrade...
-	echo %random%%random%%random%>bin\hash.txt
+	echo %random%%random%%random%>"%~dp0\bin\hash.txt"
 )
 
-set/p current_upgrade_hash=<bin\hash.txt
+set/p current_upgrade_hash=<"%~dp0\bin\hash.txt"
 if /i "!new_upgrade_hash!"=="!current_upgrade_hash!" echo You already have the latest version. && pause && exit /b
 
 ::the actual upgrade
 
 ::make dirs
 if not exist docs md docs
-if exist temp\changelog.txt del /f /q changelog.txt
-if exist temp\bget.bat del /f /q temp\bget.bat
-if exist temp\hash.txt del /f /q temp\hash.txt
-call :download -!upgrade_method! "!bget_location!#%cd%\temp\bget.bat"
-call :download -!upgrade_method! "!upgrade_hash_location!#%cd%\temp\hash.txt"
-call :download -!upgrade_method! "!changelog_location!#%cd%\temp\changelog.txt"
-call :download -!upgrade_method! "!readme_location!#%cd%\temp\readme.txt"
-if not exist "temp\hash.txt" echo Failed to get the Bget hash. && pause && exit /b
-if not exist "temp\changelog.txt" echo Failed to get the changelog. && pause && exit /b
-if not exist "temp\bget.bat" echo Failed to get Bget's latest version. && pause && exit /b
+if exist "%~dp0\temp\changelog.txt" del /f /q "%~dp0\changelog.txt"
+if exist "%~dp0\temp\bget.bat" del /f /q "%~dp0\temp\bget.bat"
+if exist "%~dp0\temp\hash.txt" del /f /q "%~dp0\temp\hash.txt"
+call :download -!upgrade_method! "!bget_location!#%~dp0\temp\bget.bat"
+call :download -!upgrade_method! "!upgrade_hash_location!#%~dp0\temp\hash.txt"
+call :download -!upgrade_method! "!changelog_location!#%~dp0\temp\changelog.txt"
+call :download -!upgrade_method! "!readme_location!#%~dp0\temp\readme.txt"
+if not exist "%~dp0\temp\hash.txt" echo Failed to get the Bget hash. && pause && exit /b
+if not exist "%~dp0\temp\changelog.txt" echo Failed to get the changelog. && pause && exit /b
+if not exist "%~dp0\temp\bget.bat" echo Failed to get Bget's latest version. && pause && exit /b
 
 ::move downloaded files
-move /Y "temp\bget.bat"
-move /Y "temp\hash.txt" "bin\hash.txt"
-move /Y "temp\changelog.txt" "docs\changelog.txt"
-move /Y "temp\readme.txt" "docs\readme.txt"
+move /Y "%~dp0\temp\bget.bat"
+move /Y "%~dp0\temp\hash.txt" "%~dp0\bin\hash.txt"
+move /Y "%~dp0\temp\changelog.txt" "%~dp0\docs\changelog.txt"
+move /Y "%~dp0\temp\readme.txt" "%~dp0\docs\readme.txt"
 
 ::start changelog
-start /max notepad docs\changelog.txt
+start /max /d "%~dp0" notepad "docs\changelog.txt"
 
 ::delete self
-start /B del /f /q %~nx0
+start /B del /f /q "%~dp0\%~nx0"
 pause
 exit
 
@@ -72,7 +72,7 @@ exit
 if /i "%~1"=="-bits" (
 	set /a rnd=%random%
 	for /f "tokens=1,2 delims=#" %%w in ("%~2") do (
-		bitsadmin /transfer Bget!rnd! /download /priority HIGH "%%w" "%%x"
+		bitsadmin /transfer Bget!rnd! /download /priority HIGH "%%w" "%%x" >nul
 	)
 	set rnd=
 	set bits_string=
@@ -81,19 +81,19 @@ if /i "%~1"=="-bits" (
 ::Jscript download function.
 ::Download.js was made by jsjoberg @ https://gist.github.com/jsjoberg/8203376
 if /i "%~1"=="-js" (
-	if not exist "bin\download.js" (
+	if not exist "%~dp0\bin\download.js" (
 		choice /c yn /n /m  "Bget's JS download function could not be found. Download? [(Y)es/(N)o]"
 		if "!errorlevel!"=="2" exit /b
 		if "!errorlevel!"=="1" (
-		if not exist bin md bin
-			call :download -bits "https://raw.githubusercontent.com/jahwi/bget/master/bin/download.js#%cd%\bin\download.js"
-			if not exist bin\download.js echo An error occured when downloading the JS function. && pause && exit /b
+		if not exist "%~dp0\bin" md "%~dp0\bin"
+			call :download -bits "https://raw.githubusercontent.com/jahwi/bget/master/bin/download.js#%~dp0\bin\download.js"
+			if not exist "%~dp0\bin\download.js" echo An error occured when downloading the JS function. && exit /b
 		)
 
 	)
-	if exist "bin\download.js" (
+	if exist "%~dp0\bin\download.js" (
 		for /f "tokens=1,2 delims=#" %%e in ("%~2") do (
-			cscript //NoLogo //e:Jscript bin\download.js "%%e" "%%f"
+			cscript //NoLogo //e:Jscript "%~dp0\bin\download.js" "%%e" "%%f"
 		)
 	)
 	exit /b
@@ -101,22 +101,18 @@ if /i "%~1"=="-js" (
 ::Curl download function
 if /i "%~1"=="-curl" (
 	call :checkcurl
-	if "!missing_curl!"=="yes" (
-		set upgrade_method=bits
-		echo Defaulting to BITSADMIN...
-		call :download -bits "%~2"
-		exit /b
-	)
+	if "!missing_curl!"=="yes" exit /b
 	for /f "tokens=1,2 delims=#" %%b in ("%~2") do (
 		curl -s "%%b" -o "%%c"
 	)
 exit /b
 )
 
+::powershell -Command wget "%%b" -OutFile "%%~sc"
 ::powershell download function
 if /i "%~1"=="-ps" (
 	for /f "tokens=1,2 delims=#" %%b in ("%~2") do (
-	powershell -Command wget "%%b" -OutFile "%%~sc"
+	Powershell.exe -command "(New-Object System.Net.WebClient).DownloadFile('%%b','%%~sc')"
 	)
 	exit /b
 )
@@ -124,19 +120,19 @@ if /i "%~1"=="-ps" (
 ::vbs thingy
 ::vbs script gotten from http://semitwist.com/articles/article/view/downloading-files-from-plain-batch-with-zero-dependencies
 if /i "%~1"=="-vbs" (
-	if not exist "bin\download.vbs" (
+	if not exist "%~dp0\bin\download.vbs" (
 		choice /c yn /n /m  "Bget's VBS download function could not be found. Download? [(Y)es/(N)o]"
 		if "!errorlevel!"=="2" exit /b
 		if "!errorlevel!"=="1" (
-		if not exist bin md bin
-		call :download -bits "https://raw.githubusercontent.com/jahwi/bget/master/bin/download.vbs#%cd%\bin\download.vbs"
-		if not exist bin\download.vbs echo An error occured when downloading the VBS function. && pause && exit /b
+		if not exist "%~dp0\bin" md "%~dp0\bin"
+		call :download -bits "https://raw.githubusercontent.com/jahwi/bget/master/bin/download.vbs#%~dp0\bin\download.vbs"
+		if not exist "%~dp0\bin\download.vbs" echo An error occured when downloading the VBS function. && exit /b
 		)
 
 	)
-	if exist "bin\download.vbs" (
+	if exist "%~dp0\bin\download.vbs" (
 		for /f "tokens=1,2 delims=#" %%e in ("%~2") do (
-			cscript //NoLogo //e:VBScript bin\download.vbs "%%e" "%%f"
+			cscript //NoLogo //e:VBScript "%~dp0\bin\download.vbs" "%%e" "%%f"
 		)
 		exit /b
 	)
